7a511197f7658af87b1c6dccda8620792e6199cf
Navneet Prajapati <navneetprajapati26@gmail.com>
2022-10-23 23:28:02 +0530

    commit after long day

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_rivesion/columns_and_rows.dart';
 import 'package:flutter_rivesion/container.dart';
 import 'package:flutter_rivesion/expanded.dart';
+import 'package:flutter_rivesion/gesture_detector.dart';
+import 'package:flutter_rivesion/gridview.dart';
+import 'package:flutter_rivesion/image.dart';
+import 'package:flutter_rivesion/listview.dart';
+import 'package:flutter_rivesion/login.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Color(0xffffff)));
   runApp(const MyApp());
 }
 
@@ -20,14 +29,14 @@ class MyApp extends StatelessWidget {
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // "hot reload" (press "r" in the console where you ran "flutter run",gjhc
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
-      home: ExpandedPage(),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
-
